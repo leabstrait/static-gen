07fc301b2cc28824a90123184902d28384340a79
Labin Ojha <leabstrait@gmail.com>
2024-03-03 21:18:14 +0000

    modify cli

--- src/staticgen/cli.py
+++ src/staticgen/cli.py
@@ -1,30 +1,30 @@
 import argparse
 import shutil
-import pkg_resources
 import os
+import importlib.resources
 
 
 def init_project(destination):
     # If destination is not provided or is '.', use the current directory
-    if destination is None or destination == '.':
+    if destination is None or destination == ".":
         destination = os.getcwd()
 
     # Check if the specified directory already exists
-    if os.path.exists(destination) and os.listdir(destination):
-        print(f"Error: Directory '{destination}' already exists and is not empty.")
-        return
-
-    os.makedirs(destination)
-
-    # Logic for initializing a starter project
-    # Path to the directory containing the starter project files
-    starter_project_dir = pkg_resources.resource_filename(
-        "staticgen", "starter_project"
-    )
+    if os.path.exists(destination):
+        if os.listdir(destination):
+            print(f"Error: Directory '{destination}' already exists and is not empty.")
+            return
 
     try:
+        import staticgen
+
+        os.makedirs(destination, exist_ok=True)
         # Copy starter project files to the destination directory
-        shutil.copytree(starter_project_dir, destination, dirs_exist_ok=True)
+        shutil.copytree(
+            os.path.join(importlib.resources.files(staticgen), "starter_project"),
+            destination,
+            dirs_exist_ok=True,
+        )
         print(f"Starter project initialized in: {destination}")
     except Exception as e:
         print(f"Error initializing starter project: {e}")
@@ -33,9 +33,7 @@ def init_project(destination):
 def main():
     parser = argparse.ArgumentParser(description="Static Site Generator CLI")
     parser.add_argument("command", choices=["init", "gen"], help="Command to execute")
-    parser.add_argument(
-        "--destination", help="Destination directory for init command"
-    )
+    parser.add_argument("--destination", help="Destination directory for init command")
 
     args = parser.parse_args()
 
@@ -43,6 +41,7 @@ def main():
         init_project(args.destination)
     elif args.command == "gen":
         from staticgen import staticgen
+
         staticgen.main()
     else:
         print("Invalid command")
